9176b63e7e250b453ae05dc650ae391fa09302ca
moonrailgun <moonrailgun@gmail.com>
2016-06-22 15:28:40 +0800

    屏幕闪烁功能
    ADD 屏幕闪烁功能

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import spidev
 import time
 
 Max7219_pinCS = 24
-#data = [[0x3c,0x42,0x42,0x42,0x42,0x42,0x42,0x3c]]
-#data = [[0xff,0x00,0x00,0x00,0x00,0x00,0x00,0xff]]
-#data = [[0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff]]
+Button_inputPin = 20
+Button_VCCPin = 21
+
 numList = [
 [0x3C,0x42,0x42,0x42,0x42,0x42,0x42,0x3C],# 0
 [0x10,0x30,0x50,0x10,0x10,0x10,0x10,0x7C],# 1
@@ -20,6 +20,8 @@ numList = [
 [0x00,0x3E,0x22,0x22,0x3E,0x22,0x22,0x3E],# 8
 [0x3E,0x22,0x22,0x3E,0x02,0x02,0x02,0x3E],# 9
 ]
+full = [0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff]
+clear = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
 
 spi = spidev.SpiDev()
 spi.open(0,0)#bus, device
@@ -48,6 +50,13 @@ def write(leftData, rightData, size = 8):
 		_data.append(leftData[i])
 		writeByte(_data)
 
+def blink(times = 2):
+	for _ in range(0,times):
+		write(full, full)
+		time.sleep(0.5)
+		write(clear, clear)
+		time.sleep(0.5)
+
 def showNum(num):
 	if not isinstance(num, int):
 		return
@@ -55,7 +64,7 @@ def showNum(num):
 	if(_num > 100):
 		_num = _num % 100
 	leftNum = 0
-	if(_num > 10):
+	if(_num >= 10):
 		leftNum = _num // 10
 	rightNum = _num % 10
 	write(numList[leftNum], numList[rightNum])
@@ -66,20 +75,30 @@ def setup():
 	GPIO.setmode(GPIO.BCM) # 使用BCM编码
 	GPIO.setup(Max7219_pinCS, GPIO.OUT) # 设定为输出通道
 
+	#设置外围电路
+	GPIO.setup(Button_VCCPin, GPIO.OUT)
+	GPIO.output(Button_VCCPin, GPIO.HIGH)
+	GPIO.setup(Button_inputPin, GPIO.IN)
+
 	initMAX7219()
 
 def loop():
+	num = 0
+	blink()
 	while(True):
-		showNum(12)
-		time.sleep(1)
-		showNum(14)
-		time.sleep(1)
-		showNum(25)
-		time.sleep(1)
-		showNum(99)
-		time.sleep(1)
-		showNum(1)
-		time.sleep(1)
+		if(GPIO.input(Button_inputPin) == GPIO.LOW):
+			print("按钮按下")
+			while (True):
+				if(GPIO.input(Button_inputPin) == GPIO.HIGH):
+					if(num >= 100):
+						num -= 100
+					print("按钮弹起，显示数字%d" % num)
+					showNum(num)
+					num += 1
+					break
+				time.sleep(0.1)
+		time.sleep(0.1)
+
 
 
 if __name__ == '__main__':
